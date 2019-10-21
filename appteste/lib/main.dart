f5942087b5b8d7a146ab42dea5b51533b999961e
alinevitoriasi <alinevitoriabsi@gmail.com>
2019-10-21 11:25:39 -0300

    Requisiçao de usuario com Flutter

--- appteste/lib/main.dart
+++ appteste/lib/main.dart
@@ -1,82 +1,79 @@
+import 'dart:async';
 import 'dart:convert';
-import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main(){
-    runApp(MinhaApp());
+Future<User> fetchPost() async {
+  final response =
+      await http.get('http://localhost:3333/user/5da6fdf79acebe19f90a3d32s');
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    return User.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Falha ao carregar um post');
+  }
 }
 
-class MinhaApp extends StatelessWidget {
+class User {
+  final String id;
+  final String email;
+  final  int v;
+
+  User({ this.id,
+        this.email,
+        this.v,});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json["_id"],
+      email: json["email"],
+      v: json["__v"],
+    );
+  }
+}
+
+void main() => runApp(MyApp(post: fetchPost()));
+
+class MyApp extends StatelessWidget {
+  final Future<User> post;
+
+  MyApp({Key key, this.post}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'LinCar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-    List data;
-    Future<String> getJSONData() async {
-      final String url = "https://unsplash.com/napi/photos/Q14J2k8VE3U/related";
-
-      var response = await http.get(url);
-      
-       data= json.decode(response.body)['results'];
-      print(data);
-    }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      home: Scaffold(
         appBar: AppBar(
           title: Text('LinCar'),
           centerTitle: true,
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.black,
         ),
-        body: _listView(),
+        body: Center(
+          child: FutureBuilder<User>(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(' email : ' + snapshot.data.email),
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              // By default, show a loading spinner
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
     );
   }
-
-_listView(){
-  return ListView.builder(
-    itemCount: data ==null?0: data.length,
-    itemBuilder: (context,index){
-      return _exibirImagem(data[index]);
-    }
-    );
-}
- _exibirImagem(dynamic item)=> Container(
-      decoration: BoxDecoration(
-        color: Colors.white
-      ),
-      margin: const EdgeInsets.all(10),
-      child: Column(
-          children: <Widget>[
-            CachedNetworkImage(
-              imageUrl: item['urls']['small'],
-            )
-          ],
-      ),
- );
-
-
-  @override
-  void initState(){
-    super.initState();
-    this.getJSONData();
-  }
-}
-
-class _exibirImagem {
 }
